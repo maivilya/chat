190d495eae9a0133dc19dda6b55b2fc49286df14
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-12-15 19:39:21 +0500

    test chat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import Client.Client;
+import Server.Server;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        new Client(server);
+        new Client(server);
     }
 }
